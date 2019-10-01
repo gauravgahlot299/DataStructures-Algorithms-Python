5c9c1016700fb1aa5e6b6ea0eb205ce52efd25f7
ggahlotInformatica <ggahlot@informatica.com>
2019-10-01 09:20:13 +0530

    Four Number Sum

---
+++ FourNumberSum.py
@@ -0,0 +1,24 @@
+a = [12, 3, 1, 2, -6, 5, -8, 6, -2, -1, 4]
+b = 18
+result = []
+a.sort()
+for idx, i in enumerate(a[:-3]):
+    c = a[idx + 1:];
+    for secondary_idx, j in enumerate(c[:-2]):
+        sumToFind = b - (i + j)
+        left = secondary_idx + 1
+        right = len(c) - 1
+        while left < right:
+            if c[left] + c[right] == sumToFind:
+                result.append([i, j, c[left], c[right]])
+                left += 1
+                right -= 1
+            else:
+                if c[left] + c[right] > sumToFind:
+                    right -= 1
+                else:
+                    left += 1
+if len(result) == 0:
+    print(-1)
+else:
+    print(result)
